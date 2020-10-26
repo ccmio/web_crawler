d837da558ec3cfdb6fe97924952eb3aabc576186
chenxi <chenxi4ever@163.com>
2020-10-26 11:28:32 +0800

    modify weibo.py

--- weibo.py
+++ weibo.py
@@ -7,7 +7,7 @@ headers = {
     Chrome/77.0.3865.90 Safari/537.36',
     'Referer': 'https://m.weibo.cn/u/2830678474'
 }
-# just for test!
+print("just for test!")
 
 def get_one_page(offset):
     params = {
